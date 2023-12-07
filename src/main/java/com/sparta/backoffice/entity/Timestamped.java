c75339ac1d3009e55b61f54abd4fe8d3cece2adb
JEONGYOUNGGYU <zkdlf90@gmail.com>
2023-12-07 10:23:38 +0900

    FEAT : 게시글 작성, 조회, 수정, 삭제 기능 구현 (+USER, POST)

--- src/main/java/com/sparta/backoffice/entity/Timestamped.java
+++ src/main/java/com/sparta/backoffice/entity/Timestamped.java
@@ -14,12 +14,16 @@ import java.time.LocalDateTime;
 public abstract class Timestamped {
 
     @CreatedDate
-    @Column(updatable = false)
+    @Column(updatable = false)          // update 가 되지 않게 막아준다.
     @Temporal(TemporalType.TIMESTAMP)
     private LocalDateTime createdAt;
 
-    @LastModifiedDate
+    @LastModifiedDate                   // 변경이 생길때 마다 변경시간을 적용
     @Column
-    @Temporal(TemporalType.TIMESTAMP)
+    @Temporal(TemporalType.TIMESTAMP)   // Date나 Calendar 의 날짜데이터를 맵핑할 때 사용함
     private LocalDateTime modifiedAt;
+
+    // DATE : 2023-01-01
+    // TIME : 20:21:14
+    // TIMESTAMP : 2023-01-01 20:21:14.9989889
 }
