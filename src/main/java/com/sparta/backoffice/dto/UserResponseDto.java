bc7b11644b00fb5f3318583051171a33289e327d
guxo2795 <guxo1211@gmail.com>
2023-12-08 19:58:50 +0900

    FIX : ADMIN_TOKEN 추가 및 수정

--- src/main/java/com/sparta/backoffice/dto/UserResponseDto.java
+++ src/main/java/com/sparta/backoffice/dto/UserResponseDto.java
@@ -1,11 +1,9 @@
 package com.sparta.backoffice.dto;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
 import com.sparta.backoffice.entity.User;
-import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 
-@JsonInclude(JsonInclude.Include.NON_NULL)
+
 @Getter
 public class UserResponseDto extends CommonResponseDto{
     private String nickname;
